abbe98e9200cb46e1755b070422e392c48021bbd
keerthi <jangamkeerthi26@gmail.com>
2024-03-26 18:46:07 +0530

    Add Main Entrees related food menu data

--- StaticData/MainEntreesData.js
+++ StaticData/MainEntreesData.js
@@ -1,86 +1,87 @@
 export const MainEntrees = [
   {
-    type: "MAIN ENTREES",
-    data: [
-      { name: "GRILLED FAROE ISLAND SALMON" },
-      { description: "quinona, oyster mushrooms, brussels sprout leaves" },
-      { price: "$26" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "MAIN ENTREES",
+    about: {
+      item: "GRILLED FAROE ISLAND SALMON",
+      description: "quinona, oyster mushrooms, brussels sprout leaves",
+      price: "$26",
+      Diet: "Dietary Information",
+      itemImage: require("../assets/logoo.png"),
+    },
   },
   {
-    type: "MAIN ENTREES",
-    data: [
-      { name: "PAN ROASTED DUCK BREAST" },
-      { description: "herbed farro, orange-frisee salad, honey gastrique" },
-      { price: "$29" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "MAIN ENTREES",
+    about: {
+      item: "PAN ROASTED DUCK BREAST",
+      description: "herbed farro, orange-frisee salad, honey gastrique",
+      price: "$29",
+      Diet: "Dietary Information",
+    },
   },
   {
-    type: "MAIN ENTREES",
-    data: [
-      { name: "CRISPY DUCK IN PORT CHERRY SAUCE" },
-      {
-        description:
-          "roasted turnips, parnips, rutabaga and carrots with cornmeal, johnnycake wrapped duck confit, bok choy",
-      },
-      { price: "$36" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "MAIN ENTREES",
+    about: {
+      item: "CRISPY DUCK IN PORT CHERRY SAUCE",
+
+      description:
+        "roasted turnips, parnips, rutabaga and carrots with cornmeal, johnnycake wrapped duck confit, bok choy",
+
+      price: "$36",
+
+      Diet: "Dietary Information",
+    },
   },
   {
-    type: "MAIN ENTREES",
-    data: [
-      { name: "CASSOULET" },
-      { description: "garlic and herb sausage, duck confit, cannellini beans" },
-      { price: "$19" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "MAIN ENTREES",
+    about: {
+      item: "CASSOULET",
+      description: "garlic and herb sausage, duck confit, cannellini beans",
+      price: "$19",
+      Diet: "Dietary Information",
+    },
   },
   {
-    type: "MAIN ENTREES",
-    data: [
-      { name: "TENDERLOIN OF BEEF WELLINGTON" },
-      {
-        description:
-          "foie gras, spinach, duxelles  ~ Add Bearnaise, Red Wine, or Bordelaise sauce for $2 extra",
-      },
-      { price: "$48" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "MAIN ENTREES",
+    about: {
+      name: "TENDERLOIN OF BEEF WELLINGTON",
+
+      description:
+        "foie gras, spinach, duxelles  ~ Add Bearnaise, Red Wine, or Bordelaise sauce for $2 extra",
+
+      price: "$48",
+      Diet: "Dietary Information",
+    },
   },
   {
-    type: "MAIN ENTREES",
-    data: [
-      { name: "HERB ROASTED PORK" },
-      {
-        description:
-          "with Creamy Polenta, Sun-Dried Tomato Jus @ Feta Gremolata",
-      },
-      { price: "$28" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "MAIN ENTREES",
+    about: {
+      item: "HERB ROASTED PORK",
+
+      description: "with Creamy Polenta, Sun-Dried Tomato Jus @ Feta Gremolata",
+
+      price: "$28",
+      Diet: "Dietary Information",
+    },
   },
   {
-    type: "MAIN ENTREES",
-    data: [
-      { name: "CLASSIC NEW YORK SIRLOIN" },
-      {
-        description:
-          "~ Add Jumbo Gulf Shrimp with garlic, white wine, lemon for $9",
-      },
-      { price: "$26" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "MAIN ENTREES",
+    about: {
+      item: "CLASSIC NEW YORK SIRLOIN",
+
+      description:
+        "~ Add Jumbo Gulf Shrimp with garlic, white wine, lemon for $9",
+
+      price: "$26",
+      Diet: "Dietary Information",
+    },
   },
   {
-    type: "MAIN ENTREES",
-    data: [
-      { name: "IDAHO RED TROUT" },
-      { description: "green lentils, chanterelle mushrooms, Waldoboro turnip" },
-      { price: "$25" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "MAIN ENTREES",
+    about: {
+      item: "IDAHO RED TROUT",
+      description: "green lentils, chanterelle mushrooms, Waldoboro turnip",
+      price: "$25",
+      Diet: "Dietary Information",
+    },
   },
 ];

773d3bcb2549eb4d4bb6b3a0854bca1e4434e7ad
keerthi <jangamkeerthi26@gmail.com>
2024-03-26 18:47:51 +0530

    Create desserts related component

---
+++ components/DessertsList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+const DessertsList = () => {
+  return (
+    <ScrollView style={styles.container}>
+      
+    </ScrollView>
+  );
+};
+
+export default DessertsList;

c04069042365499e1dd09f56d27a0fbdc92f6768
keerthi <jangamkeerthi26@gmail.com>
2024-03-26 23:35:54 +0530

    Render Main entrees related data

--- components/MainEntreesList.js
+++ components/MainEntreesList.js
@@ -1,27 +1,48 @@
 import React from "react";
-import { FlatList, StyleSheet, Text, View,Image } from "react-native";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 import { MainEntrees } from "../StaticData/MainEntreesData";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 22,
+    backgroundColor: "white",
+    padding: 20,
+    flex:1
+  },
+  header: {
+    alignSelf: "center",
+    fontSize: 20,
+    fontWeight: "400",
+    color: "red",
+    margin: 10,
   },
   item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+    color: "brown",
+  },
+  description: {
+    fontStyle: "italic",
+  },
+  menu: {
+    flexWrap: "wrap",
+    flexDirection: "column",
   },
 });
 
 const MainEntreesList = () => {
   return (
     <View style={styles.container}>
+      <Text style={styles.header}>MAIN ENTREES</Text>
       <FlatList
         data={MainEntrees}
-        renderItem={({item}) => <View><Text style={styles.item}>{item.about.item}</Text>
-    <Text>{item.about.description}</Text>
-    <Image source={item.about.itemImage}/></View>}
+        renderItem={({ item }) => (
+          <View style={styles.menu}>
+            <View>
+              <Text style={styles.item}>{item.about.itemName}</Text>
+              <Text style={styles.description}>{item.about.description}</Text>
+              <Text>{item.about.Diet}</Text>
+            </View>
+            <Text>{item.about.price}</Text>
+          </View>
+        )}
       />
     </View>
   );

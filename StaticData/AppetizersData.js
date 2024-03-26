42813e8c2981dd6d54e98dd09df9221f0c3ee39f
keerthi <jangamkeerthi26@gmail.com>
2024-03-26 18:44:24 +0530

    Add Appetizers related food menu data

--- StaticData/AppetizersData.js
+++ StaticData/AppetizersData.js
@@ -1,23 +1,22 @@
 export const Appetizers = [
   {
-    type: "APPETIZERS",
-    data: [
-      { name: "DAY BOAT SCALLOPS" },
-      { description: "fennel, prosciutto, black sauce" },
-      { price: "$14" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "APPETIZERS",
+    data: {
+      item: "DAY BOAT SCALLOPS",
+      description: "fennel, prosciutto, black sauce",
+      price: "$14",
+      Diet: "Dietary Information",
+    },
   },
   {
-    type: "APPETIZERS",
-    data: [
-      { name: "DEVILS ON HORSEBACK" },
-      {
-        description:
-          "bacon wrapped dates stuffed with feta, balsamic reduction",
-      },
-      { price: "$12" },
-      { Diet: "Dietary Information" },
-    ],
+    title: "APPETIZERS",
+    data: {
+      item: "DEVILS ON HORSEBACK",
+
+      description: "bacon wrapped dates stuffed with feta, balsamic reduction",
+
+      price: "$12",
+      Diet: "Dietary Information",
+    },
   },
 ];

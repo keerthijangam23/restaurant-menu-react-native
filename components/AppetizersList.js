0fd70c811979e2ce59037f436043aa22b8404a52
keerthi <jangamkeerthi26@gmail.com>
2024-03-26 18:47:05 +0530

    Create Appetizers related component

---
+++ components/AppetizersList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FlatList, StyleSheet, Text, View,Image } from "react-native";
+import { Appetizers } from "../StaticData/AppetizersData";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  logoo: {
+    width: 90,
+    height: 50,
+    marginTop: 20,
+  },
+});
+const logoImage = require("../assets/logoo.png");
+const AppetizersList = () => {
+  return (
+    
+  );
+};
+
+export default AppetizersList;

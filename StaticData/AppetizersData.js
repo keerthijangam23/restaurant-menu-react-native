73473a06941f54e76c489fac6e5c52952f7897c3
keerthi <jangamkeerthi26@gmail.com>
2024-03-26 12:47:33 +0530

    Add static data folder

---
+++ StaticData/AppetizersData.js
@@ -0,0 +1,23 @@
+export const Appetizers = [
+  {
+    type: "APPETIZERS",
+    data: [
+      { name: "DAY BOAT SCALLOPS" },
+      { description: "fennel, prosciutto, black sauce" },
+      { price: "$14" },
+      { Diet: "Dietary Information" },
+    ],
+  },
+  {
+    type: "APPETIZERS",
+    data: [
+      { name: "DEVILS ON HORSEBACK" },
+      {
+        description:
+          "bacon wrapped dates stuffed with feta, balsamic reduction",
+      },
+      { price: "$12" },
+      { Diet: "Dietary Information" },
+    ],
+  },
+];
---
+++ StaticData/DesssertsData.js
@@ -0,0 +1,67 @@
+export const Desserts = [
+  {
+    type: "DESSERTS",
+    data: [
+      { name: "TRES LECHES CAKE" },
+      { description: "Strawberry compote, Strawberry balsamic" },
+      { price: "$9" },
+      { Diet: "Dietary Information" },
+    ],
+  },
+  {
+    type: "DESSERTS",
+    data: [
+      { name: "KEY LIME PUDDING" },
+      { description: "Chantilly cream & wafer cookies" },
+      { price: "$8" },
+      { Diet: "Dietary Information" },
+    ],
+  },
+  {
+    type: "DESSERTS",
+    data: [
+      { name: "HOUSE MADE ICE CREAM" },
+      { description: "Blood orange sherbet" },
+      { price: "$9" },
+      { Diet: "Dietary Information" },
+    ],
+  },
+  {
+    type: "DESSERTS",
+    data: [
+      {
+        name: "CARROT CAKE",
+      },
+      {
+        description:
+          "Made with carrot, pineapple, coconuts, and raisins, with cream cheese frosting",
+      },
+      { price: "$6" },
+      { Diet: "Dietary Information" },
+    ],
+  },
+  {
+    type: "DESSERTS",
+    data: [
+      { name: "BERRY NAPOLEON" },
+      {
+        description:
+          "Fresh mixed berries served in a white chocolate merinque basket with a passion fruit anglaise",
+      },
+      { price: "$7" },
+      { Diet: "Dietary Information" },
+    ],
+  },
+  {
+    type: "DESSERTS",
+    data: [
+      { name: "KEY LIME PIE" },
+      {
+        description:
+          "Coconut semifreddo, raspberry whipped creme franche, honey tuile & raspberry reduction",
+      },
+      { price: "$8" },
+      { Diet: "Dietary Information" },
+    ],
+  },
+];

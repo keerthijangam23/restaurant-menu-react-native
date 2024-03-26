a298e353bfe43b945bf010b98762189601398570
keerthi <jangamkeerthi26@gmail.com>
2024-03-26 23:34:30 +0530

    Render appetizers related list data

--- components/AppetizersList.js
+++ components/AppetizersList.js
@@ -1,23 +1,56 @@
 import React from "react";
-import { FlatList, StyleSheet, Text, View,Image } from "react-native";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 import { Appetizers } from "../StaticData/AppetizersData";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems:'center',
-    justifyContent:'center'
+    backgroundColor: "white",
+    padding: 20,
+    flex:1
   },
-  logoo: {
-    width: 90,
-    height: 50,
-    marginTop: 20,
+  header: {
+    alignSelf: "center",
+    fontSize: 20,
+    fontWeight: "400",
+    color: "red",
+    marginTop: 10,
   },
+  item: {
+    color: "brown",
+  },
+  description: {
+    fontStyle: "italic",
+  },
+  menu: {
+    flexWrap: "wrap",
+    flexDirection: "column",
+  },
+  caption:{
+    alignSelf:"center",
+    fontStyle:"italic",
+    paddingBottom:10
+  }
 });
-const logoImage = require("../assets/logoo.png");
+
 const AppetizersList = () => {
   return (
-    
+    <View style={styles.container}>
+      <Text style={styles.header}>APPETIZERS</Text>
+      <Text style={styles.caption}>Our local menu changes daily</Text>
+      <FlatList
+        data={Appetizers}
+        renderItem={({ item }) => (
+          <View style={styles.menu}>
+            <View>
+              <Text style={styles.item}>{item.about.itemName}</Text>
+              <Text style={styles.description}>{item.about.description}</Text>
+              <Text>{item.about.Diet}</Text>
+            </View>
+            <Text>{item.about.price}</Text>
+          </View>
+        )}
+      />
+    </View>
   );
 };
 

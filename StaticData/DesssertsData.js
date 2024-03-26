5c03eab88f62b4ca5a2768872c1fb6e3a128df31
keerthi <jangamkeerthi26@gmail.com>
2024-03-26 23:09:44 +0530

    change the data

--- StaticData/DesssertsData.js
+++ StaticData/DesssertsData.js
@@ -1,8 +1,8 @@
 export const Desserts = [
   {
     title: "DESSERTS",
-    data: {
-      item: "TRES LECHES CAKE",
+    about: {
+      itemName: "TRES LECHES CAKE",
       description: "Strawberry compote, Strawberry balsamic",
       price: "$9",
       Diet: "Dietary Information",
@@ -10,8 +10,8 @@ export const Desserts = [
   },
   {
     title: "DESSERTS",
-    data: {
-      item: "KEY LIME PUDDING",
+    about: {
+      itemName: "KEY LIME PUDDING",
       description: "Chantilly cream & wafer cookies",
       price: "$8",
       Diet: "Dietary Information",
@@ -19,8 +19,8 @@ export const Desserts = [
   },
   {
     title: "DESSERTS",
-    item: {
-      name: "HOUSE MADE ICE CREAM",
+    about: {
+      itemName: "HOUSE MADE ICE CREAM",
       description: "Blood orange sherbet",
       price: "$9",
       Diet: "Dietary Information",
@@ -28,8 +28,8 @@ export const Desserts = [
   },
   {
     title: "DESSERTS",
-    data: {
-      item: "CARROT CAKE",
+    about: {
+      itemName: "CARROT CAKE",
       description:
         "Made with carrot, pineapple, coconuts, and raisins, with cream cheese frosting",
       price: "$6",
@@ -38,8 +38,8 @@ export const Desserts = [
   },
   {
     title: "DESSERTS",
-    data: {
-      item: "BERRY NAPOLEON",
+    about: {
+      itemName: "BERRY NAPOLEON",
       description:
         "Fresh mixed berries served in a white chocolate merinque basket with a passion fruit anglaise",
       price: "$7",
@@ -48,8 +48,8 @@ export const Desserts = [
   },
   {
     title: "DESSERTS",
-    data: {
-      item: "KEY LIME PIE",
+    about: {
+      itemName: "KEY LIME PIE",
       description:
         "Coconut semifreddo, raspberry whipped creme franche, honey tuile & raspberry reduction",
       price: "$8",

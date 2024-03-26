6582adca3893b64fd0d4951d1ab2e61500b189a7
keerthi <jangamkeerthi26@gmail.com>
2024-03-26 23:35:17 +0530

    Render desserts related list of data

--- components/DessertsList.js
+++ components/DessertsList.js
@@ -1,23 +1,50 @@
 import React from "react";
-import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
+import { Desserts } from "../StaticData/DesssertsData";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 22,
+    backgroundColor: "white",
+    padding: 20,
+    flex:1
+  },
+  header: {
+    alignSelf: "center",
+    fontSize: 20,
+    fontWeight: "400",
+    color: "red",
+    margin: 10,
   },
   item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+    color: "brown",
+  },
+  description: {
+    fontStyle: "italic",
+  },
+  menu: {
+    flexWrap: "wrap",
+    flexDirection: "column",
   },
 });
 
 const DessertsList = () => {
   return (
-    <ScrollView style={styles.container}>
-      
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.header}>DESSERTS</Text>
+      <FlatList
+        data={Desserts}
+        renderItem={({ item }) => (
+          <View style={styles.menu}>
+            <View>
+              <Text style={styles.item}>{item.about.itemName}</Text>
+              <Text style={styles.description}>{item.about.description}</Text>
+              <Text>{item.about.Diet}</Text>
+            </View>
+            <Text>{item.about.price}</Text>
+          </View>
+        )}
+      />
+    </View>
   );
 };
 

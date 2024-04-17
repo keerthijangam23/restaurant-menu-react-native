33476ec3b00adab04538dea2958c3b98bd73f5c9
keerthi <jangamkeerthi26@gmail.com>
2024-04-17 08:29:08 +0530

    Add Dashboard component to render other components

---
+++ components/Dashboard.tsx
@@ -0,0 +1,18 @@
+import { ScrollView, View,StyleSheet } from "react-native";
+import AboutComponent from "./AboutComponent";
+import SectionListMenuData from "./SectionListMenuData";
+export default function Dashboard() {
+  return (
+    <View style={styles.container}>
+      <AboutComponent />
+      <SectionListMenuData />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 70,
+        marginBottom: -280,
+        flex: 1,
+      },
+})

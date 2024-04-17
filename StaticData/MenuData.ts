7bc5ecdab19822943589aacf3ba407ea4af65001
keerthi <jangamkeerthi26@gmail.com>
2024-04-17 08:26:33 +0530

    Add static data sectionwise and remove other static data files

---
+++ StaticData/MenuData.ts
@@ -0,0 +1,164 @@
+export const MenuData = [
+  {
+    title: "APPETIZERS",
+    data: [
+      {
+        itemName: "DAY BOAT SCALLOPS",
+        description: "fennel, prosciutto, black sauce",
+        price: "$14",
+        Diet: "Dietary Information",
+        itemImage: require("../assets/9.jpg"),
+        type: "APPETIZERS",
+      },
+      {
+        itemName: "DEVILS ON HORSEBACK",
+
+        description:
+          "bacon wrapped dates stuffed with feta, balsamic reduction",
+
+        price: "$12",
+        complimentary: "~ extra bacon $3",
+        Diet: "Dietary Information",
+        newImage: require("../assets/newImage.jpeg"),
+        type: "APPETIZERS",
+      },
+    ],
+  },
+
+  {
+    title: "DESSERTS",
+    data: [
+      {
+        itemName: "TRES LECHES CAKE",
+        description: "Strawberry compote, Strawberry balsamic sdnf ",
+        price: "$9",
+        complimentary:
+          "~Jorge Ordonez & Co. Victoria 2-Malaga, Spain $10.00 glass",
+        // itemImage: require("../assets/11.jpg"),
+        type: "DESSERTS",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "KEY LIME PUDDING",
+        description: "Chantilly cream & wafer cookies",
+        price: "$8",
+        // Diet: "Dietary Information",
+        // itemImage: require("../assets/12.jpg"),
+        type: "DESSERTS",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "HOUSE MADE ICE CREAM",
+        description: "Blood orange sherbet",
+        price: "$9",
+        itemImage: require("../assets/13.jpg"),
+        type: "DESSERTS",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "CARROT CAKE",
+        description:
+          "Made with carrot, pineapple, coconuts, and raisins, with cream cheese frosting",
+        price: "$6",
+        // Diet: "Dietary Information",
+        // itemImage: require("../assets/14.jpg"),
+        type: "DESSERTS",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "BERRY NAPOLEON",
+        description:
+          "Fresh mixed berries served in white chocolate merinque basket with a passion fruit anglaise",
+        price: "$7",
+        // Diet: "Dietary Information",
+        // itemImage: require("../assets/15.jpg"),
+        type: "DESSERTS",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "KEY LIME PIE",
+        description:
+          "Coconut semifreddo, raspberry whipped creme franche, honey tuile & raspberry reduction",
+        price: "$8",
+        // Diet: "Dietary Information",
+        // itemImage: require("../assets/16.jpg"),
+        type: "DESSERTS",
+        Diet: "Dietary Information",
+      },
+    ],
+  },
+  {
+    title: "MAIN ENTREES",
+    data: [
+      {
+        itemName: "GRILLED FAROE ISLAND SALMON",
+        description: "quinona, oyster mushrooms, brussels sprout leaves",
+        price: "$26",
+        itemImage: require("../assets/1.jpeg"),
+        type: "MAIN ENTREES",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "PAN ROASTED DUCK BREAST",
+        description: "herbed farro, orange-frisee salad, honey gastrique",
+        price: "$29",
+        // itemImage: require("../assets/2.jpg"),
+        type: "MAIN ENTREES",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "CRISPY DUCK IN PORT CHERRY SAUCE",
+        description:
+          "roasted turnips, parnips, rutabaga and carrots with cornmeal, johnnycake wrapped duck confit, bok choy",
+        price: "$36",
+        // itemImage: require("../assets/3.jpeg"),
+        type: "MAIN ENTREES",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "CASSOULET",
+        description: "garlic and herb sausage, duck confit, cannellini beans",
+        price: "$19",
+        itemImage: require("../assets/4.jpg"),
+        type: "MAIN ENTREES",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "TENDERLOIN OF BEEF WELLINGTON",
+        description: "foie gras, spinach, duxelles ",
+        price: "$48",
+        complimentary:
+          "~ Add Bearnaise, Red Wine, or Bordelaise sauce for $2 extra",
+        // itemImage: require("../assets/5.jpg"),
+        type: "MAIN ENTREES",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "HERB ROASTED PORK",
+        description:
+          "with Creamy Polenta, Sun-Dried Tomato Jus @ Feta Gremolata",
+        price: "$28",
+        // itemImage: require("../assets/6.jpg"),
+        type: "MAIN ENTREES",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "CLASSIC NEW YORK SIRLOIN",
+        complimentary:
+          "~ Add Jumbo Gulf Shrimp with garlic, white wine, lemon for $9",
+        price: "$26",
+        // itemImage: require("../assets/7.jpg"),
+        type: "MAIN ENTREES",
+        Diet: "Dietary Information",
+      },
+      {
+        itemName: "IDAHO RED TROUT",
+        description: "green lentils, chanterelle mushrooms, Waldoboro turnip",
+        price: "$25",
+        // itemImage: require("../assets/8.jpg"),
+        type: "MAIN ENTREES",
+        Diet: "Dietary Information",
+      },
+    ],
+  },
+];

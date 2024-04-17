deca1b0a9394ceeb281a324c410e5f0b55982be5
keerthi <jangamkeerthi26@gmail.com>
2024-04-17 13:03:51 +0530

    Remove NavigationContainer in testing file

--- --tests--/AboutComponent.test.tsx
+++ --tests--/AboutComponent.test.tsx
@@ -1,52 +1,31 @@
-
 import { screen, render, fireEvent } from "@testing-library/react-native";
-import { NavigationContainer } from "@react-navigation/native";
+
 import AboutComponent from "../components/AboutComponent";
 import "@testing-library/react-native/extend-expect";
 
 describe("checking wheather AboutComponent is present or not", () => {
   it("checking resturant image rendered or not", () => {
-    render(
-      <NavigationContainer>
-        <AboutComponent />
-      </NavigationContainer>
-    );
+    render(<AboutComponent />);
     const restaurantImage = screen.getByTestId("resturant-back-ground-image");
     expect(restaurantImage).toBeDefined();
   });
   it("Checking header rendering or not", () => {
-    render(
-      <NavigationContainer>
-        <AboutComponent />
-      </NavigationContainer>
-    );
+    render(<AboutComponent />);
     const headerText = screen.getByTestId("headerText");
     expect(headerText).toBeDefined();
   });
   it("Checking logo rendering or not", () => {
-    render(
-      <NavigationContainer>
-        <AboutComponent />
-      </NavigationContainer>
-    );
+    render(<AboutComponent />);
     const logoImage = screen.getByTestId("logoImage");
     expect(logoImage).toBeDefined();
   });
   it("Checking restaurant name rendering or not", () => {
-    render(
-      <NavigationContainer>
-        <AboutComponent />
-      </NavigationContainer>
-    );
+    render(<AboutComponent />);
     const restaurantName = screen.getByText("Del Ray Gourmet");
     expect(restaurantName).toBeDefined();
   });
   it("Checking down arrow rendering or not", () => {
-    const { getByText } = render(
-      <NavigationContainer>
-        <AboutComponent />
-      </NavigationContainer>
-    );
+    const { getByText } = render(<AboutComponent />);
     const downArrow = screen.getByTestId("downArrow");
     fireEvent.press(downArrow);
     expect(getByText("Business Hours 301-555-1212")).toBeDefined();

8a005b4d15610d15ad788f2594cadfc52a5c4ed0
keerthi <jangamkeerthi26@gmail.com>
2024-04-17 08:31:15 +0530

    Add unwanted code in App .tsx file

--- App.tsx
+++ App.tsx
@@ -1,26 +1,5 @@
-import { ScrollView, View } from "react-native";
-import AboutComponent from "./components/AboutComponent";
-import { NavigationContainer } from "@react-navigation/native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import AppetizersList from "./components/AppetizersList";
-import MainEntreesList from "./components/MainEntreesList";
-import DessertsList from "./components/DessertsList";
+import Dashboard from "./components/Dashboard";
 
-const Tab = createMaterialTopTabNavigator();
 export default function App() {
-  return (
-    <NavigationContainer>
-      <AboutComponent />
-      <Tab.Navigator screenOptions={{
-            tabBarLabelStyle: { fontSize: 12, fontWeight: "500" },
-            tabBarActiveTintColor: "rgb(248, 113, 113)",
-            tabBarInactiveTintColor: "black",
-            tabBarPressColor: "lightpink",
-          }}>
-        <Tab.Screen name="Appetizers" component={AppetizersList} />
-        <Tab.Screen name="Main Entrees" component={MainEntreesList} />
-        <Tab.Screen name="Desserts" component={DessertsList} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  return <Dashboard />;
 }
